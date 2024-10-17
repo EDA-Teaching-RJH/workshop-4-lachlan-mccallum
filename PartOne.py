c1a75775548ebb097a688cb792dfe6ca693413c4
lachlan-mccallum <lm935@kent.ac.uk>
2024-10-17 13:11:42 +0000

    part 1

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,12 @@
+def main():
+    s = input("give your name in camel case ")
+    print("snake_case ", end="")
 
+    for c in s:
+        if c.islower():
+            print(c,end="")
+        elif c.isupper():
+            print ("_", c.lower(), end = "", sep = "")
+    print()
+
+main()
